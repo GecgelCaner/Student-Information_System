32c6acad14b731e943cf173168337cce5d5fb299
GecgelCaner <gecgelcaner.10@gmail.com>
2023-05-20 17:26:06 +0300

    Metotlar eklendi

--- Student_Information_System/src/Main.java
+++ Student_Information_System/src/Main.java
@@ -2,5 +2,11 @@
 public class Main {
     public static void main(String[] args) {
 
+        Teacher t1 = new Teacher("Mahmut" , "TRH" , "05450000000");
+        Teacher t2 = new Teacher("caner" , "FZK" , "05770000000");
+        Course tarih = new Course("Tarih","101", "TRH" );
+
+        tarih.addTeacher(t1);
+
     }
 }
--- Student_Information_System/src/Student.java
+++ Student_Information_System/src/Student.java
@@ -1,8 +1,12 @@
 public class Student {
 
+    Course c1;
+    Course c2;
+    Course c3;
     String name ;
     String studentNo ;
     String classes ;
-
+    double average;
+    boolean isPass;
 
 }
